bcd83827fd947a6c19243a452dad25df4f6a9f91
Auroiah Morgan <amwebdev86@outlook.com>
2020-12-12 05:27:58 -0500

    elint

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
-import { useEffect, useState } from 'react';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App({ login }) {
   const [data, setData] = useState(null);
+  const [query, setQuery] = useState('1'); //grabs first pokemon
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
+    setLoading(true);
+    fetch(`https://pokeapi.co/api/v2/pokemon-species/${query}/`)
+      .then(response => {
+        if (!response.ok) throw new Error('Not Found');
+       return response.json();
+      }).then(setData)
+      .then(setLoading(false))
+      .catch(setError)
     
-  })
+  }, [query]);
+  if (loading) return <h1 className='loading-text'>Loading...</h1>;
+  if (error) return <h1>{JSON.stringify(error, null, 2)}</h1>
+  console.log(data)
   return (
-    <div className="App">
-     <h1>Pokedex</h1>
+    <div className='App'>
+      <h1>Pokedex</h1>
+      <form>
+        <input
+          type='text'
+          placeholder='Enter name or ID'
+          
+        />
+        <input className='btn search-btn' type='submit'></input>
+      </form>
     </div>
   );
 }
