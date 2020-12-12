0b801fe35d273487793552a1379e854dd7a0eae1
Auroiah Morgan <amwebdev86@outlook.com>
2020-12-12 06:49:52 -0500

    add search

--- src/App.js
+++ src/App.js
@@ -11,44 +11,41 @@ function App({ login }) {
   const handleSubmit = (e) => {
     e.preventDefault();
     let newQuery = e.target[0].value;
-
-    console.log(newQuery);
+    setQuery(newQuery);
+    
 
   };
-  
+
   
   useEffect(() => {
     setLoading(true);
     fetch(`https://pokeapi.co/api/v2/pokemon-species/${query}/`)
       .then((response) => {
-        if (!response.ok) {
-          let myError = new Error('Not Found');
-          throw myError;
-        }
+       
         return response.json();
       })
       .then(setData)
       .then(setLoading(false))
-      .catch(err => {
-        setError(err);
-        console.error(err);
-      });
+      .catch((err)=>setError(err));
   }, [query]);
+
   if (loading) return <h1 className='loading-text'>Loading...</h1>;
-  if (error) return <h1>Error:{JSON.stringify(error, null, 2)}</h1>;
-  // console.log(`the query: ${query}`);
-  // console.dir(data)
-  return (
+  if (error) return <pre>Error:{JSON.stringify(error, 'error', 2)}</pre>;
+  
+  return data ? (
     <div className='App'>
       <h1>Pokedex</h1>
       <form onSubmit={handleSubmit }>
         <input type='text'  placeholder='Enter name or ID' />
         <input className='btn search-btn' type='submit'></input>
       </form>
+      <h1>{data.name}</h1>
     </div>
     
       
-  );
-}
+  ) : (<>
+  <pre>There was an issue</pre>
+    </>);
+    }
 
-export default App;
+    export default App;
