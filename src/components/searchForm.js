5e960107618abd083cf361bbc9f779059810deaf
Auroiah Morgan <amwebdev86@outlook.com>
2020-12-18 05:05:36 -0500

    Update searchForm.js

--- src/components/searchForm.js
+++ src/components/searchForm.js
@@ -2,15 +2,20 @@ import React from 'react';
 import './css/SearchForm.css';
 
 export default function Search({ handleSubmit }) {
-  const form = (<form className='poke-search' onSubmit={handleSubmit}>
-    <input type='text' placeholder='Enter name or ID' />
-    <input
-      className=' App-btn-link-1 App-shadow'
-      type='submit'
-      value='Search'
-    />
-  </form>);
+  const form = (
+    <form className='poke-search' onSubmit={handleSubmit}>
+      <label>
+        Search:
+        <input type='text' placeholder='Enter name or ID' />
+      </label>
 
-  
+      <input
+        className=' App-btn-link-1 App-shadow'
+        type='submit'
+        value='Search'
+      />
+    </form>
+  );
+
   return <div>{form}</div>;
 }
